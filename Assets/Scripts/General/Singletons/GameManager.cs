5816b0627ca5991c5aa771ff336b3c75681f70cc
Paulo H <paulo@ninito.me>
2021-04-08 12:53:49 -0300

    Add Singleton script, Game Manager and Menu Manager

---
+++ Assets/Scripts/General/Singletons/GameManager.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : SingletonBase<GameManager>
+{
+    #region Unity Callbacks
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+    
+    #endregion
+
+    #region Public Methods
+
+    /// <summary>
+    /// Loads a given scene
+    /// </summary>
+    /// <param name="sceneName">The build name of the scene</param>
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    /// <summary>
+    /// Quits the game application
+    /// </summary>
+    public void QuitApplication()
+    {
+        Application.Quit();
+    }
+    
+    #endregion
+}
