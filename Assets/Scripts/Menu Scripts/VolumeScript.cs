75290bdc33c8f35dd34d64d3316c34d8517503d9
Renzo <renzo.diniz@sga.pucminas.br>
2021-04-15 09:05:59 -0300

    Updates Volume Sliders
    Adds proper sliders to all volume settings

--- Assets/Scripts/Menu Scripts/VolumeScript.cs
+++ Assets/Scripts/Menu Scripts/VolumeScript.cs
@@ -7,7 +7,11 @@ public class VolumeScript : MonoBehaviour
 {
     #region Public Fields
 
-    public AudioMixer Mixer;
+    [SerializeField]
+    private string ExposedParam;
+    [SerializeField]
+    private AudioMixer Mixer;
+    
     // Choose which mixer will have this method applied to it
 
     #endregion
@@ -20,7 +24,7 @@ public class VolumeScript : MonoBehaviour
     /// <param name="sliderValue">Turns the min and max value of the slider into a number between 0 and 80 decibels allowing for a more accurate sound setting.</param>
     public void SetVolume(float sliderValue)
     {
-        Mixer.SetFloat("MasterVol", Mathf.Log10(sliderValue) * 20);
+        Mixer.SetFloat(ExposedParam, Mathf.Log10(sliderValue) * 20);
     }
 
     #endregion
