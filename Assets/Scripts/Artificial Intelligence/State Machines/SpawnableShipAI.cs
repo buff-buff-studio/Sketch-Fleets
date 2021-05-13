100b95ab0951f7a741ff3604519ec0b7bf28776f
Paulo H <paulo@ninito.me>
2021-05-12 23:25:20 -0300

    Add SpawnableShipAI state machine

--- Assets/Scripts/Artificial Intelligence/State Machines/SpawnableShipAI.cs
+++ Assets/Scripts/Artificial Intelligence/State Machines/SpawnableShipAI.cs
@@ -1,17 +1,43 @@
 ﻿using ManyTools.UnityExtended;
 using SketchFleets.Enemies;
 using SketchFleets.Entities;
+using UnityEngine;
 
 namespace SketchFleets.AI
 {
     /// <summary>
     /// A state machine that controls the AI of a ship
     /// </summary>
+    [RequireComponent(typeof(SpawnedShip))]
     public class SpawnableShipAI : StateMachine
     {
         #region Private Fields
 
+        private GameObject player;
         private SpawnedShip ship;
+        private Camera mainCamera;
+
+        #endregion
+
+
+        #region Properties
+
+        public GameObject Player => player;
+        public SpawnedShip Ship => ship;
+        public Camera MainCamera => mainCamera;
+
+        #endregion
+        
+        #region Unity Callbacks
+
+        protected override void Start()
+        {
+            mainCamera = Camera.main;
+            player = GameObject.FindWithTag("Player");
+            ship = GetComponent<SpawnedShip>();
+            
+            base.Start();
+        }
 
         #endregion
     }
