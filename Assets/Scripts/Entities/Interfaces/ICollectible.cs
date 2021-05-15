1f59f9e6a51af426d1b5bfd2a52b161f366137de
Paulo H <paulo@ninito.me>
2021-05-15 16:23:52 -0300

    Add interface for collectible elements, ICollectible

---
+++ Assets/Scripts/Entities/Interfaces/ICollectible.cs
@@ -0,0 +1,13 @@
+﻿namespace SketchFleets
+{
+    /// <summary>
+    /// A class that controls a collectible entity
+    /// </summary>
+    public interface ICollectible
+    {
+        /// <summary>
+        /// Applies all necessary effects upon collection
+        /// </summary>
+        public void Collect();
+    }
+}
