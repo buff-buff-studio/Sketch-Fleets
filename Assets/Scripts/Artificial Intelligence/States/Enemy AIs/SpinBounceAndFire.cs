78b719c34db94aaa6d375f36de6d5861216867b8
Paulo H <paulo@ninito.me>
2021-06-12 22:40:59 -0300

    Fix green ship

--- Assets/Scripts/Artificial Intelligence/States/Enemy AIs/SpinBounceAndFire.cs
+++ Assets/Scripts/Artificial Intelligence/States/Enemy AIs/SpinBounceAndFire.cs
@@ -11,10 +11,19 @@ namespace SketchFleets.AI
         #region Private Fields
 
         [SerializeField]
+        private FloatReference bounceCooldown = new FloatReference(4f);
+        [SerializeField]
         private FloatReference rotationSpeedModifier = new FloatReference(4f);
 
         private Transform cachedTransform;
         private Vector3 moveDirection;
+        private float bounceTimer;
+
+        #endregion
+
+        #region Properties
+
+        protected bool CanBounce => bounceTimer <= 0f;
 
         #endregion
 
@@ -28,7 +37,7 @@ namespace SketchFleets.AI
             base.Enter();
 
             cachedTransform = transform;
-            moveDirection = cachedTransform.right;
+            moveDirection = cachedTransform.up;
         }
 
         /// <summary>
@@ -41,15 +50,12 @@ namespace SketchFleets.AI
             float temporalSpeed = AI.Ship.Attributes.Speed * Time.deltaTime;
 
             cachedTransform.Translate(moveDirection * temporalSpeed, Space.World);
-
-            temporalSpeed *= rotationSpeedModifier;
-            
-            cachedTransform.Rotate(
-                new Vector3(0f, 0f, temporalSpeed));
+            Spin(temporalSpeed);
 
             AI.Ship.Fire();
-        }
 
+            bounceTimer -= Time.deltaTime * Time.timeScale;
+        }
 
         #endregion
 
@@ -57,10 +63,39 @@ namespace SketchFleets.AI
 
         private void OnCollisionEnter2D(Collision2D other)
         {
+            Bounce(!other.gameObject.CompareTag("bullet"));
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        /// <summary>
+        /// Spins the ship
+        /// </summary>
+        private void Spin(float speed)
+        {
+            speed *= rotationSpeedModifier;
+            cachedTransform.Rotate(new Vector3(0f, 0f, speed));
+        }
+        
+        /// <summary>
+        /// Bounces the ship
+        /// </summary>
+        /// <param name="ignoreCooldown">Whether to ignore the bounce cooldown</param>
+        private void Bounce(bool ignoreCooldown = true)
+        {
+            if (!ignoreCooldown)
+            {
+                if (!CanBounce) return;
+            }
+
             float random = Random.Range(0.1f, 0.3f);
             Vector3 randomDirection = new Vector3(random, random, 0f);
             moveDirection *= -1f;
             moveDirection += randomDirection;
+
+            bounceTimer = bounceCooldown;
         }
 
         #endregion
