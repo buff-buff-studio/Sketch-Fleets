3d8173a372bf641604d5bde1872feab7900b2bda
Paulo Oliveira <paulo@ninito.me>
2021-10-02 12:27:18 -0300

    Remove and optimize assets

--- Assets/Game/Scripts/Other/InitializeApplication.cs
+++ Assets/Game/Scripts/Other/InitializeApplication.cs
@@ -10,10 +10,6 @@ namespace SketchFleets
     {
         #region Private Fields
 
-        [Header("Game Values")]
-        [SerializeField]
-        private RefreshRate targetFramerate;
-
         [Header("Loading")]
         [SerializeField]
         private string menuSceneName;
@@ -29,14 +25,9 @@ namespace SketchFleets
         // Start is called before the first frame update
         private void Start()
         {
-            SetTargetFramerate();
             LoadMainMenu();
 #if !UNITY_EDITOR
-            CreateBackTraceClient();
-#endif
-
-#if DEVELOPMENT_BUILD
-            Debug.Log(Application.targetFrameRate);
+            Instantiate(backTraceClient);
 #endif
         }
 
@@ -45,41 +36,11 @@ namespace SketchFleets
         #region Private Methods
 
         /// <summary>
-        ///     Sets the application's target framerate
-        /// </summary>
-        private void SetTargetFramerate()
-        {
-            Application.targetFrameRate = (int)targetFramerate;
-        }
-
-        /// <summary>
         ///     Loads the game's main menu
         /// </summary>
         private void LoadMainMenu()
         {
             SceneManager.LoadSceneAsync(menuSceneName);
-        }
-
-        /// <summary>
-        ///     Creates a backtrace client for error-tracking
-        /// </summary>
-        private void CreateBacktraceClient()
-        {
-            Instantiate(backTraceClient);
-        }
-
-        #endregion
-
-        #region Private Enums
-
-        /// <summary>
-        ///     A simple refresh rate enumeration
-        /// </summary>
-        private enum RefreshRate
-        {
-            PlatformDefault = -1,
-            Capped30 = 30,
-            Capped60 = 60
         }
 
         #endregion
