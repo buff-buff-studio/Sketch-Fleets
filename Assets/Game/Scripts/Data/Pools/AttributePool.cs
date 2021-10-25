437f5decc6f7ff5807c23d51dc403d35d824c5ae
Paulo Oliveira <paulo@ninito.me>
2021-10-24 22:44:21 -0300

    Seal AttributePool class

--- Assets/Game/Scripts/Data/Pools/AttributePool.cs
+++ Assets/Game/Scripts/Data/Pools/AttributePool.cs
@@ -5,7 +5,7 @@ namespace SketchFleets.Data
 {
     [CreateAssetMenu(fileName = CreateMenus.attributePoolFileName, menuName = CreateMenus.attributePoolMenuName,
         order = CreateMenus.attributePoolOrder)]
-    public class AttributePool : WeightedPool<Attributes>
+    public sealed class AttributePool : WeightedPool<Attributes>
     {
         
     }
