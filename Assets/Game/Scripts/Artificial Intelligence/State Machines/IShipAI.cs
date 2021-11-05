7fa0d0bfacaad50cbacc8f8fb091fd6bd24374aa
Paulo Oliveira <paulo@ninito.me>
2021-11-04 22:25:54 -0300

    Add basic IShipAI interface to ShipAIs

---
+++ Assets/Game/Scripts/Artificial Intelligence/State Machines/IShipAI.cs
@@ -0,0 +1,23 @@
+﻿using SketchFleets.Data;
+
+namespace SketchFleets.AI
+{
+    /// <summary>
+    /// An interface that provides basic functionality for AI.
+    /// </summary>
+    public interface IShipAI
+    {
+        public ShipAttributes.Faction Faction { get; }
+        
+        /// <summary>
+        /// Sets the ship's faction
+        /// </summary>
+        /// <param name="faction">The faction to set the ship to</param>
+        public void SetFaction(ShipAttributes.Faction faction);
+        
+        /// <summary>
+        /// Flips the ship's faction. Neutral ships cannot be flipped.
+        /// </summary>
+        public void FlipFaction();
+    }
+}
