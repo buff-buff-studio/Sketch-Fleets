02bdc6b67c35252ec32cb382b093ff6396499a74
Paulo Oliveira <paulo@ninito.me>
2021-11-13 12:17:05 -0300

    Seal MainMenuScript

--- Assets/Game/Scripts/User Interface/MainMenuScript.cs
+++ Assets/Game/Scripts/User Interface/MainMenuScript.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 using UnityEngine.UI;
 
 
-public class MainMenuScript : MonoBehaviour
+public sealed class MainMenuScript : MonoBehaviour
 {   
     #region Public Fields
     public Button buttonContinue;
