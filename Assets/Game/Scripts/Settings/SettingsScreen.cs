eb4fc472c3df62ee734c98557301f2ba28912eca
Paulo Oliveira <paulo@ninito.me>
2021-11-13 12:15:36 -0300

    Clean up SettingsScreen window

--- Assets/Game/Scripts/Settings/SettingsScreen.cs
+++ Assets/Game/Scripts/Settings/SettingsScreen.cs
@@ -9,23 +9,34 @@ using SketchFleets.SettingsSystem;
 namespace SketchFleets
 {
     #region Utils
-    public class OptionData : TMP_Dropdown.OptionData
+
+    public sealed class OptionData : TMP_Dropdown.OptionData
     {
         #region Private Fields
+
         private string _value;
+
         #endregion
 
         #region Properties
-        public string value { get => _value; }
+
+        public string value
+        {
+            get => _value;
+        }
+
         #endregion
 
         #region Constructors
+
         public OptionData(string name, string value) : base(name)
         {
             this._value = value;
         }
+
         #endregion
     }
+
     #endregion
 
     /// <summary>
@@ -34,6 +45,7 @@ namespace SketchFleets
     public class SettingsScreen : MonoBehaviour
     {
         #region Dropdowns and Sliders IDS
+
         private const int LANGUAGE = 0;
         private const int GRAPHICS_QUALITY = 1;
         private const int RESOLUTION = 2;
@@ -44,13 +56,15 @@ namespace SketchFleets
 
         private const int WINDOWS_MODE = 6;
         private const int VSYNC = 7;
-        
+
         private const int DEBUG_MODE = 9;
-        
+
         private const int CONTROLS_MODE = 10;
+
         #endregion
 
         #region Public Fields
+
         public TMP_Dropdown languageDropdown;
         public LocalizableSelector graphicsQuality;
         public TMP_Dropdown resolutionDropdown;
@@ -61,12 +75,15 @@ namespace SketchFleets
         public Slider volumeMaster;
         public Slider volumeMusic;
         public Slider volumeSfx;
+
         #endregion
 
         #region Unity Callbacks
+
         private void OnEnable()
         {
             #region Language
+
             if (languageDropdown != null)
             {
                 languageDropdown.ClearOptions();
@@ -74,54 +91,47 @@ namespace SketchFleets
                 int value = 0;
                 int i = 0;
                 string lang = Settings.GetObject().language;
+
                 foreach (Language s in LanguageManager.GetAllLanguages())
                 {
                     data.Add(new OptionData(s.Name, s.Code));
-                    if (s.Code == lang)
-                        value = i;
+                    if (s.Code == lang) value = i;
                     i++;
                 }
+
                 languageDropdown.AddOptions(data);
                 languageDropdown.value = value;
             }
+
             #endregion
 
             #region Graphics Quality
+
             if (graphicsQuality != null)
             {
                 graphicsQuality.UpdateLocalization();
                 graphicsQuality.value = Settings.GetObject().graphicsQuality;
             }
-            #endregion
 
-            #region Screen
-            /*
-            if(resolutionDropdown != null)
-                resolutionDropdown.value = Settings.Get<int>("resolution");
-            if(windowsModeDropdown != null)
-            {
-                windowsModeDropdown.GetComponent<LocalizableDropdown>().UpdateLocalization();
-                windowsModeDropdown.value = Settings.Get<int>("winMode");
-            }
-            if(vsyncToggle != null)
-                vsyncToggle.isOn = Settings.Get<bool>("vsync");
-            SettingsManager.RefreshWindow();
-            */
             #endregion
 
             #region Volume
+
             volumeMaster.value = Settings.GetObject().volumeMaster;
             volumeMusic.value = Settings.GetObject().volumeMusic;
             volumeSfx.value = Settings.GetObject().volumeSfx;
+
             #endregion
 
             #region Gameplay
+
             if (controlsModeDropdown != null)
             {
                 controlsModeDropdown.value = Settings.GetObject().controlMode;
             }
-            if (debugModeToggle != null)
-                debugModeToggle.isOn = Settings.GetObject().debugMode;
+
+            if (debugModeToggle != null) debugModeToggle.isOn = Settings.GetObject().debugMode;
+
             #endregion
 
             //FullScreenMode.Windowed
@@ -132,73 +142,56 @@ namespace SketchFleets
             switch (dropdownId)
             {
                 case LANGUAGE:
-                    {
-                        string language = ((OptionData)languageDropdown.options[languageDropdown.value]).value;
-                        LanguageManager.ChangeLanguage(language);
-                        Settings.GetObject().language = language;
-                        Settings.Save();
-                    }
+                {
+                    string language = ((OptionData)languageDropdown.options[languageDropdown.value]).value;
+                    LanguageManager.ChangeLanguage(language);
+                    Settings.GetObject().language = language;
+                    Settings.Save();
+                }
                     break;
 
                 case GRAPHICS_QUALITY:
-                    {
-                        QualitySettings.SetQualityLevel(graphicsQuality.value);
-                        Settings.GetObject().graphicsQuality = graphicsQuality.value;
-                        Settings.Save();
-                    }
+                {
+                    QualitySettings.SetQualityLevel(graphicsQuality.value);
+                    Settings.GetObject().graphicsQuality = graphicsQuality.value;
+                    Settings.Save();
+                }
                     break;
-
-                /*
-                case RESOLUTION:
-                {
-                    SettingsManager.SetResolution(resolutionDropdown.value);
-                }
-                break;
-
-                case WINDOWS_MODE:
-                {
-                    SettingsManager.SetWindowMode(windowsModeDropdown.value);
-                }
-                break;
-
-                case VSYNC:
-                {
-                    Settings.Set<bool>("vsync",vsyncToggle.isOn);
-                    SettingsManager.RefreshWindow();
-                }
-                break;
-                */
+                
                 case VOLUME_MASTER:
-                    {
-                        Settings.GetObject().volumeMaster = volumeMaster.value;
-                        SettingsManager.SetVolume(SettingsManager.instance.volumeMasterParam, SettingsManager.instance.volumeMasterMixer, volumeMaster.value);
-                        Settings.Save();
-                    }
+                {
+                    Settings.GetObject().volumeMaster = volumeMaster.value;
+                    SettingsManager.SetVolume(SettingsManager.instance.volumeMasterParam,
+                        SettingsManager.instance.volumeMasterMixer, volumeMaster.value);
+                    Settings.Save();
+                }
                     break;
 
                 case VOLUME_MUSIC:
-                    {
-                        Settings.GetObject().volumeMusic = volumeMusic.value;
-                        SettingsManager.SetVolume(SettingsManager.instance.volumeMusicParam, SettingsManager.instance.volumeMusicMixer, volumeMusic.value);
-                        Settings.Save();
-                    }
+                {
+                    Settings.GetObject().volumeMusic = volumeMusic.value;
+                    SettingsManager.SetVolume(SettingsManager.instance.volumeMusicParam,
+                        SettingsManager.instance.volumeMusicMixer, volumeMusic.value);
+                    Settings.Save();
+                }
                     break;
 
                 case VOLUME_SFX:
-                    {
-                        Settings.GetObject().volumeSfx = volumeSfx.value;
-                        SettingsManager.SetVolume(SettingsManager.instance.volumeSfxParam, SettingsManager.instance.volumeSfxMixer, volumeSfx.value);
-                        Settings.Save();
-                    }
+                {
+                    Settings.GetObject().volumeSfx = volumeSfx.value;
+                    SettingsManager.SetVolume(SettingsManager.instance.volumeSfxParam,
+                        SettingsManager.instance.volumeSfxMixer, volumeSfx.value);
+                    Settings.Save();
+                }
                     break;
 
                 case DEBUG_MODE:
-                    {
-                        Settings.GetObject().debugMode = debugModeToggle.isOn;
-                        Settings.Save();
-                    }
+                {
+                    Settings.GetObject().debugMode = debugModeToggle.isOn;
+                    Settings.Save();
+                }
                     break;
-                
+
                 case CONTROLS_MODE:
                 {
                     Settings.GetObject().controlMode = controlsModeDropdown.value;
@@ -207,6 +200,7 @@ namespace SketchFleets
                     break;
             }
         }
+
         #endregion
     }
 }
