0b47fa1da3d29b462d6d6b27993d7842251a4637
Paulo Oliveira <paulo@ninito.me>
2021-11-30 13:09:39 -0300

    Clean up Purple Bullet script

--- Assets/ZombieBulletController.cs
+++ Assets/ZombieBulletController.cs
@@ -1,34 +1,49 @@
-using System.Collections;
-using System.Collections.Generic;
 using SketchFleets.AI;
-using SketchFleets.Data;
 using UnityEngine;
 
 namespace SketchFleets
 {
-    public class ZombieBulletController : BulletController
+    /// <summary>
+    /// A class that controls the zombie 'mind control' bullet
+    /// </summary>
+    public sealed class ZombieBulletController : BulletController
     {
         protected override void DealDamageToTarget(bool directDamage, GameObject target)
         {
-            if (target.CompareTag("Player") || target.CompareTag("PlayerSpawn"))
+            if (target.CompareTag("Player"))
             {
                 if (Attributes.IgnorePlayer) return;
-                target.GetComponent<IDamageable>()?.Damage(GetDamage(directDamage));
-                if(target.GetComponent<IShipAI>().Faction == ShipAttributes.Faction.Friendly)
-                    target.GetComponent<IShipAI>().FlipFaction();
+                ApplyDamage(directDamage, target);
             }
-            else
+            else if (target.CompareTag("PlayerSpawn") || target.CompareTag("Enemy"))
             {
-                if (target.CompareTag("Enemy"))
-                {
-                    if(target.GetComponent<IShipAI>().Faction == ShipAttributes.Faction.Hostile)
-                        target.GetComponent<IShipAI>().FlipFaction();
-                }
-
-                target.GetComponent<IDamageable>()?.Damage(GetDamage(directDamage));
+                ApplyDamage(directDamage, target);
+                FlipTargetsFaction(target);
             }
             
             Submerge();
+        }
+
+        /// <summary>
+        /// Flips the target's faction
+        /// </summary>
+        /// <param name="target">The target to flip the faction of</param>
+        private static void FlipTargetsFaction(GameObject target)
+        {
+            if (target.TryGetComponent(out IShipAI targetAI))
+            {
+                targetAI.FlipFaction();
+            }
+        }
+
+        /// <summary>
+        /// Applies damage to the target
+        /// </summary>
+        /// <param name="directDamage">Whether damage is direct</param>
+        /// <param name="target">The target to apply damage to</param>
+        private void ApplyDamage(bool directDamage, GameObject target)
+        {
+            target.GetComponent<IDamageable>()?.Damage(GetDamage(directDamage));
         }
     }
 }
