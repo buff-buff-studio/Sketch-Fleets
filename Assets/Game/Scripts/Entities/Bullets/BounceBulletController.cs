42c7ac344b8168c6c3a1e6f39194e87364313705
Paulo Oliveira <paulo@ninito.me>
2021-12-01 20:47:40 -0300

    Fix BounceBullet emerge not restoring scale

--- Assets/Game/Scripts/Entities/Bullets/BounceBulletController.cs
+++ Assets/Game/Scripts/Entities/Bullets/BounceBulletController.cs
@@ -15,7 +15,7 @@ namespace SketchFleets
         private float maxBounceLife = 4;
 
         private float bounceLife;
-        private readonly Vector3 maxScale = new Vector3(.5f, .5f, .5f);
+        private Vector3 originalScale;
 
         #endregion
 
@@ -24,12 +24,8 @@ namespace SketchFleets
         public override void Emerge(Vector3 position, Quaternion rotation)
         {
             bounceLife = maxBounceLife;
+            transform.localScale = originalScale;
 
-            if (transform.localScale.x < maxScale.x)
-            {
-                transform.localScale = maxScale;
-            }
-            
             base.Emerge(position, rotation);
         }
 
@@ -37,16 +33,14 @@ namespace SketchFleets
 
         #region Unity Callbacks
 
+        private void Awake()
+        {
+            originalScale = transform.localScale;
+        }
+
         protected override void Update()
         {
-            if (PlayerBuletVelocity == 0)
-            {
-                Move(Vector3.up * Attributes.Speed, Space.Self);
-            }
-            else
-            {
-                Move(Vector3.up * PlayerBuletVelocity, Space.World);
-            }
+            Move(Vector3.up * Attributes.Speed, Space.Self);
         }
 
         private void OnCollisionEnter2D(Collision2D other)
@@ -58,11 +52,6 @@ namespace SketchFleets
         {
             Bounce(col.gameObject.CompareTag("bullet"));
             DealDamageToTarget(false, col.gameObject);
-        }
-
-        private void OnDestroy()
-        {
-            Debug.Log($"Bounce Bullet ({gameObject}) destroyed!");
         }
 
         #endregion
