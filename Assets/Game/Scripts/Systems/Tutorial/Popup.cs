33231487d7078c92ca8ca76cb22dd4704ddc38a6
Paulo Oliveira <paulo@ninito.me>
2021-11-30 21:07:23 -0300

    Add Popup script

---
+++ Assets/Game/Scripts/Systems/Tutorial/Popup.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace SketchFleets.Systems.Tutorial
+{
+    /// <summary>
+    /// A simple class that handles popups
+    /// </summary>
+    public sealed class Popup : MonoBehaviour
+    {
+        #region Public Methods
+
+        /// <summary>
+        /// Destroys the object containing the component
+        /// </summary>
+        public void DestroySelf()
+        {
+            Destroy(gameObject);
+        }
+
+        #endregion
+    }
+}
