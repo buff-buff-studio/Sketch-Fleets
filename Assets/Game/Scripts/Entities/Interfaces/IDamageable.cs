439bbfb84bc847ba3681453d7739339fc326e6aa
Paulo Oliveira <paulo@ninito.me>
2021-12-02 10:22:16 -0300

    Add DamageContext to IDamageable calls

--- Assets/Game/Scripts/Entities/Interfaces/IDamageable.cs
+++ Assets/Game/Scripts/Entities/Interfaces/IDamageable.cs
@@ -1,14 +1,19 @@
+using SketchFleets.Systems.DeathContext;
+
 namespace SketchFleets
 {
     public interface IDamageable
     {
+        public DamageContext LatestDamageContext { get; set; }
+
         /// <summary>
         /// Damages the Damageable object by the given amount
         /// </summary>
         /// <param name="amount">The amount to damage for</param>
+        /// <param name="context">The context of the damage being applies</param>
         /// <param name="makeInvulnerable">Whether the object should be made invulnerable after the damage</param>
         /// <param name="piercing">Whether the damage should ignore defense and shields</param>
-        public void Damage(float amount, bool makeInvulnerable = false, bool piercing = false);
+        public void Damage(float amount, DamageContext context, bool makeInvulnerable = false, bool piercing = false);
 
         /// <summary>
         /// Heals the Damageable object by the given amount
@@ -20,9 +25,10 @@ namespace SketchFleets
         /// Damages the Damageable object by the given amount
         /// </summary>
         /// <param name="amount">The amount to damage for</param>
+        /// <param name="context">The context of the damage being applied</param>
         /// <param name="pulses">Over how many 'pulses' should the damage be applied</param>
         /// <param name="time">Over how long should the pulses be spread</param>
-        public void DamageContinually(float amount, int pulses, float time);
+        public void DamageContinually(float amount, DamageContext context, int pulses, float time);
     }
 }
 
