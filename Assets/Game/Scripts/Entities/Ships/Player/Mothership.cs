9f9fc07165a94b7759201eebf6ed39809bb441ab
Paulo Oliveira <paulo@ninito.me>
2021-12-02 16:36:20 -0300

    Update Mothership to use new bullet override mechanism

--- Assets/Game/Scripts/Entities/Ships/Player/Mothership.cs
+++ Assets/Game/Scripts/Entities/Ships/Player/Mothership.cs
@@ -252,7 +252,7 @@ namespace SketchFleets.Entities
 
             // Spawns the ship
             PoolMember spawn = PoolManager.Instance.Request(shipType.Prefab);
-            spawn.GetComponent<SpawnedShip>().bulletPrefab = colorsInventory.bulletColor;
+            spawn.GetComponent<SpawnedShip>().OverrideBullet(colorsInventory.latestBullet);
             spawn.Emerge(shipSpawnPoint.position, Quaternion.identity);
             spawn.GetComponent<SpriteRenderer>().material.SetColor(redMultiplier, colorsInventory.drawColor);
             colorsInventory.UseColor();
