aa2a28e314e54af17ec0faac1694245a31187d5a
Paulo Oliveira <paulo@ninito.me>
2021-12-05 10:42:24 -0300

    Add DrawingRequirementsApplier script

---
+++ Assets/Game/Scripts/Draw Input/DrawingRequirementApplier.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+namespace SketchFleets.Systems.DrawInput
+{
+    /// <summary>
+    /// A class that applies a requirement to drawing a line.
+    /// </summary>
+    public sealed class DrawingRequirementApplier : MonoBehaviour
+    {
+        #region Private Fields
+
+        [Header("Drawing Requirements")]
+        [SerializeField]
+        private int requiredShapeIndex;
+
+        [SerializeField]
+        private bool forceEnterDrawMode;
+
+        #endregion
+
+        #region Unity Callbacks
+
+        private void Start()
+        {
+            LineDrawer drawer = FindObjectOfType<LineDrawer>(true);
+
+            if (forceEnterDrawMode)
+            {
+                Debug.Log("Forcing drawing to begin");
+                drawer.ForceBeginDraw();
+            }
+            
+            drawer.RequireOutcome(requiredShapeIndex);
+        }
+
+        #endregion
+    }
+}
