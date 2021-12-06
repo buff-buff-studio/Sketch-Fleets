c415c9ecd642703f34f138e774d3e1aa726b5496
Paulo Oliveira <paulo@ninito.me>
2021-12-06 04:09:48 -0300

    Add script that causes bullet time on enable

---
+++ Assets/Game/Scripts/Systems/Bullet Time/BulletTimeCause.cs
@@ -0,0 +1,30 @@
+﻿using SketchFleets.General;
+using UnityEngine;
+
+namespace SketchFleets.Systems
+{
+    /// <summary>
+    /// A class that causes bullet time when spawned
+    /// </summary>
+    public sealed class BulletTimeCause : MonoBehaviour
+    {
+        #region Private Fields
+
+        [Header("Bullet Time Parameters")]
+        [SerializeField]
+        private AnimationCurve timeByRealTime = new AnimationCurve();
+        [SerializeField]
+        private float bulletTimeDuration = 1.0f;
+
+        #endregion
+
+        #region Unity Callbacks
+
+        private void OnEnable()
+        {
+            LevelManager.Instance.BulletTimeManager.StartBulletTime(timeByRealTime, bulletTimeDuration);
+        }
+
+        #endregion
+    }
+}
